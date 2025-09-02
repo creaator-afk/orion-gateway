cb6774bd06482b7ee2b0907901f32a3a1141428c
Kshitij Satpathy <akuma1091@gmail.com>
2025-09-02 14:16:45 +0530

    Initial commit

---
+++ src/main/java/com/backend/oriongateway/OrionGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.backend.oriongateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrionGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrionGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/oriongateway/OrionGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.oriongateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrionGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
